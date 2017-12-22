c817346bc402f4ffb7dd82921dce7c9a0e219e69
RihanZer0 <rinat-sonic@mail.ru>
2017-12-22 15:05:27 +0300

    Add files via upload

--- homework/uni.py
+++ homework/uni.py
@@ -15,15 +15,15 @@ print(dictionary([2,3,4,5],[2,4,3]))
 if __name__ == '__main__':
     import unittest
 
-    class TestFactorialMethods(unittest.TestCase):
+    class Test_U(unittest.TestCase):
 
         def test_normal_values(self):
             self.assertEqual(dictionary([2,3,4,5],[22,33,44,55]),{2: 22, 3: 33, 4: 44, 5: 55})
             self.assertEqual(dictionary([2,3,4,5,6],[22,33,44,55]),{2: 22, 3: 33, 4: 44, 5: 55, 6: None})
 
         def test_vals_float_type(self):
-            self.assertTrue(dictionary(8,8) is None)
-            self.assertTrue(dictionary(8,[8]) is None)
+            self.assertTrue(dictionary(7.7, 7.7) is None)
+            self.assertTrue(dictionary([7.7],[7.7]), {7.7: 7.7})
 
         def test_vals_bool_type(self):
             self.assertIsNone(dictionary(False,True), None)
@@ -33,7 +33,11 @@ if __name__ == '__main__':
             self.assertEqual(dictionary(-355,-4444), None)
 
         def test_iter_objs(self):
-            self.assertIs(dictionary('fff','lol'), None )
-            self.assertIs(dictionary({43,44},{33,44}), None )
+            self.assertIs(dictionary('sss','zzz'), None )
+            self.assertIs(dictionary({33,44},{33,44}), None )
 
-    uni.main(verbosity=2)
+    unittest.main(verbosity=2)
+
+assert dictionary([2,3,4,5],[22,33,44,55]) == {2: 22, 3: 33, 4: 44, 5: 55}, ('Wrong!')
+assert dictionary([2,3,4,5,6],[22,33,44,55]) == {2: 22, 3: 33, 4: 44, 5: 55, 6: None}, ('Wrong!')
+
